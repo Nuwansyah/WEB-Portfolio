f1eb16bbfb86d25b36b682d2a4f829768fd5539b
Nuwansyah <nuwansyahbillah@gmail.com>
2026-04-15 13:44:29 +0700

    first commit

---
+++ portfolio-next/data/project.ts
@@ -0,0 +1,101 @@
+export const projects = [
+  {
+    slug: "budget-confirmation-digitalization-system",
+    title: "Budget Confirmation Digitalization System",
+    description:
+      "lore ipsum",
+    tech: ["Power Apps", "Power Automate", "SharePoint"],
+    thumnail: "",
+    media: [
+      "/images/SaliraResto/1.png"
+    ],
+  },
+  {
+    slug: "salira-resto",
+    title: "Restourant POS System", 
+    description:
+      "Lore Ipsum",
+    tech: ["C#", ".Net", "SQL", "Visual Studio"],
+    thumnail: "",
+    media: [
+      "/images/budget1.png",
+    ],
+  },
+  {
+    slug: "kampung-sebelah",
+    title: "Kampung Sebelah - 2D Farming Simulation Game",
+    description:
+      "LoreIpSHUM",
+    tech: ["C#", "Unity"],
+    thumnail: "",
+    media: [
+      "/images/budget1.png",
+    ],
+  },
+  {
+    slug: "vr-mitigation-simulation",
+    title: "VR Mitigation Simulation - Disaster Preparedness Training Game",
+    description:
+      "LoreIpSHUM",
+    tech: ["C#", "Unity", "VR Framework"],
+    thumnail: "",
+    media: [
+      "/images/budget1.png",
+    ],
+  },
+  {
+    slug: "on-your-own",
+    title: "On Your Own - 3D Top-Down Shooter Survival Game",
+    description:
+      "LoreIpSHUM",
+    tech: ["C#", "Unity"],
+    thumnail: "",
+    media: [
+      "/images/budget1.png",
+    ],
+  },
+  {
+    slug: "tree-of-life",
+    title: "Tree of Life - 3D Top Down Shooting Game",
+    description:
+      "LoreIpSHUM",
+    tech: ["C#", "Unity"],
+    thumnail: "",
+    media: [
+      "/images/budget1.png",
+    ],
+  },
+  {
+    slug: "edukomp",
+    title: "Edukomp - E-Learning Platform Prototype",
+    description:
+      "LoreIpSHUM",
+    tech: ["Next.js", "Clerk", "Aiven(PostgreSQL)", "Vercel"],
+    thumnail: "",
+    media: [
+      "/images/budget1.png",
+    ],
+  },
+  {
+    slug: "blender3d",
+    title: "Blender 3D - Interior & Environment Rendering",
+    description:
+      "LoreIpSHUM",
+    tech: ["Blender"],
+    thumnail: "",
+    media: [
+      "/images/budget1.png",
+    ],
+  },
+  {
+    slug: "minecraft-modding",
+    title: "Minecraft Modding – Custom Content & Gameplay Extensions",
+    description:
+      "LoreIpSHUM",
+    tech: ["Javascript", "Blockbench"],
+    thumnail: "",
+    media: [
+      "/images/budget1.png",
+    ],
+  }
+];
---
+++ portfolio-next/types/project.ts
@@ -0,0 +1,8 @@
+export type Project = {
+  slug: string;
+  title: string;
+  description: string;
+  tech: string[];
+  media: string[];
+  thumnail: string;
+};
